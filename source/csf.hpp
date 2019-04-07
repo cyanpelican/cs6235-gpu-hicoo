e49d196328c8a2b50f9865a1648f3a3fbf80342a
Kevin Parker <cyanpelican@users.noreply.github.com>
2019-04-07 13:45:08 -0600

    CSF tensor format

--- source/csf.hpp
+++ source/csf.hpp
@@ -2,6 +2,21 @@
 #ifndef CSF_HPP
 #define CSF_HPP
 
-// TODO, maybe
+struct CSFPoint {
+    float value;
+    unsigned int fiberX, fiberY; // the other 2 coordinates not related to the fiber
+};
+
+
+struct CSFTensor {
+    CSFPoint* points;
+    unsigned int* fiberAddresses; // should be length (numFibers+1); fiber ends are stored implicitly
+    unsigned int numFibers;
+    unsigned int width, height, depth;
+    unsigned int mode;
+};
+
+// Note - to me, the "fiber" in csf makes me feel like x and y should be implicit and z should be explicit,
+// rather than having x be implicit and y/z be explicit. But who knows.
 
 #endif
