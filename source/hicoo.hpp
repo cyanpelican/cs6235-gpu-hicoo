66c1aa82cd87c1ef63da4a1bf76878315dbd38fc
Kevin Parker <kevin.m.parker@gmail.com>
2019-04-06 23:34:33 -0600

    starting on hicoo struct

---
+++ source/hicoo.hpp
@@ -0,0 +1,22 @@
+#ifndef HICOO_HPP
+#define HICOO_HPP
+
+struct CooPoint {
+    int x, y, z;
+    float value;
+};
+
+enum PointSorting {
+    UNSORTED,
+    XYZ,
+    Z_MORTON
+};
+
+
+struct COOTensor {
+    Point* points;
+    PointSorting sorting;
+    int num_elements;
+};
+
+#endif
