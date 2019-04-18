1b071093d3ac7bf5a8a1f9c31d1cb4f4044fc152
James Brissette <brissette@sci.utah.edu>
2019-04-17 19:16:02 -0600

    Added a few lines for testing, few to go

--- source/main.cpp
+++ source/main.cpp
@@ -8,7 +8,15 @@
 #include "coo.hpp"
 #include "common.hpp"
 
+int size = 50;
+int RANDOM_SEED = 1234;
+bool cooKernal = 0, hicooKernal = 0;
 
+vector <string> tensorList;
+
+tensorList.push_back("tensorA.file");
+tensorList.push_back("tensorB.file");
+tensorList.push_back("tensorC.file");
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -18,10 +26,71 @@ int main(int argc, char *argv[]) {
 
     char *matrixName = argv[1];
     CooTensorManager Coo;
-    Coo.create(argv[1]);
+    DenseTensorManager Dense;
+    HiooTensorManager Hicoo;
+    
+    
+    CooPoint pointList[size*size*size];
+
+    //Random Dense Tensor
+    float B[size][size][size];
+    srand(RANDOM_SEED);
+    for (int i=0; i<size; i++) {
+        for (int j=0; j<size; j++) {
+            for (int k=0; k<size; k++) {
+                B[i][j][k] = rand() / 100000;
+                //Sets a random Coo Tensor instead
+                /*int idx = i*size*size + j*size + k;
+                pointList[idx].x = i;
+                pointList[idx].y = j;
+                pointList[idx].z = k;
+                pointList[idx].value = rand() / 100000;*/
+            }
+        }
+    }
+
+    //Random Dense Matrices
+    float D[size][size];
+    float C[size][size];
+    for (int i=0; i<size; i++) {
+        for (int j=0; j<size; j++) {
+            D[i][j] = rand() / 100000;
+            C[i][j] = rand() / 100000;
+        }
+    }
+
+    //Dense Test
+    //Calculate MTTKRP on CPU
+    float cpuOut[size][size];
+    //Calculate MTTKRP on GPU
+    float gpuOut[size][size];
+
+    // VERIFY CORRECTNESS BY COMPARING OUTPUTS
+    for (i=0; i<size; i++) { // minibatch size
+        for (j=0; j<size; j++) { // output feature map
+            if(abs(cpuOut[i][j]-gpuOut[i][j]) > .0001) {
+                printf("Outputs do not match! - %f vs %f\n", abs(cpuOut[i][j]), abs(gpuOut[i][j]));
+                exit(2);
+            }
+        }
+    }
+
+    //Coo Test
+    if (cooKernal) {
+        //Dense = ((CooTensor)Coo).mttkrp_
+        //((CooTensor)Coo).points_h = &pointList;
+    }
+    
+    //Hicoo Test
+    if (hicooKernal) {
+        Dense = 
+    }
+
+    //Coo.create(argv[1]);
     std::cout << ((CooTensor)Coo).numElements << std::endl;
 
-
+    
+    
     return 0;
 }
 
