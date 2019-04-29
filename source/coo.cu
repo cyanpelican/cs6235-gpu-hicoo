1597e7634f7239649ce74896013b8c71f403f188
Kevin Parker <kevin.m.parker@gmail.com>
2019-04-28 21:59:09 -0600

    more assert

--- source/coo.cu
+++ source/coo.cu
@@ -162,6 +162,10 @@ DenseMatrixManager CooTensor::mttkrp_naive_cpu(DenseMatrixManager D, DenseMatrix
     assert(d.height == L);
     assert(c.height == K);
     assert(c.width  == J);
+    assert(a.points_h != nullptr);
+    assert(values_h != nullptr);
+    assert(c.points_h != nullptr);
+    assert(d.points_h != nullptr);
 
 
     a.setSize(I, J);
