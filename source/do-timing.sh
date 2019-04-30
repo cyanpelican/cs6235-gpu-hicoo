dbd973e9c7b37969b4a2538f58c5ab1cdde5611d
Kevin Parker <kevin.m.parker@gmail.com>
2019-04-30 13:03:00 -0600

    timing scripts ready for final kernels

--- source/do-timing.sh
+++ source/do-timing.sh
@@ -5,10 +5,10 @@ set -e
 ./get-datasets.sh
 
 sbatch time-dense.sbatch
-sbatch time-flickr.sbatch
+#sbatch time-flickr.sbatch
 sbatch time-crime.sbatch #higher-order
 sbatch time-enron.sbatch #higher-order
 sbatch time-uber.sbatch #higher-order
-sbatch time-nell1.sbatch
+#sbatch time-nell1.sbatch
 sbatch time-nell2.sbatch
 sbatch time-small-tensors.sbatch
